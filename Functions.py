be4430f63f2b725a2bab282a5fb91239912b6970
CharlieJWMarsh <72220190+CharlieJWMarsh@users.noreply.github.com>
2021-10-25 00:33:37 +0100

    Written a function to form the Euclidean distance matrix from the normalised matrix.

--- Functions.py
+++ Functions.py
@@ -7,3 +7,12 @@ def normalise_ds(df):
     column_sd = np.sqrt(np.sum((df - column_mean) ** 2, axis=0) / np.shape(df)[0])
     normalised_df = (df - column_mean) / column_sd
     return normalised_df
+
+
+def form_ed_matrix(df):
+    ed_matrix = np.zeros([np.shape(df)[0], np.shape(df)[0]])
+    for i in range(0, np.shape(df)[0]):
+        for j in range(0, np.shape(df)[0]):
+            ed_matrix[i, j] = np.sqrt(np.sum((df[i, :] - df[j, :]) ** 2))
+    return ed_matrix
+
--- Main.py
+++ Main.py
@@ -3,9 +3,11 @@ import numpy as np
 import math
 import Functions as f
 
-ds = np.array([[1, 2, 3, 4, 5], [6, 13, 6, 4, 2], [3, 4, 5, 6, 7], [9, 10, 11, 12, 13], [1, 2, 3, 4, 5]])
+ds = np.array([[1, 2, 3, 4, 5], [6, 13, 6, 4, 2], [3, 4, 5, 6, 7], [9, 10, 11, 12, 13]])
 print(ds, '\n')
 
 normalised_ds = f.normalise_ds(ds)
 print(normalised_ds, '\n')
 
+ed_matrix = f.form_ed_matrix(normalised_ds)
+print(ed_matrix, '\n')
