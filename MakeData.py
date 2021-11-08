dce32fac60be594648d4e3ad3250fd9a5d546030
CharlieJWMarsh <72220190+CharlieJWMarsh@users.noreply.github.com>
2021-11-08 02:27:04 +0000

    Made the first 2 Tables inner join on just geography

--- MakeData.py
+++ MakeData.py
@@ -3,41 +3,33 @@ import pandas as pd
 import csv
 
 
+pd.set_option('display.max_columns', 100)  # or 1000
+pd.set_option('display.max_rows', 100)  # or 1000
+pd.set_option('display.max_colwidth', None)  # or 199
+
 KS101EW = pd.read_csv(r'C:\Users\charl\OneDrive\Documents\2011 Census\KS101EW.csv')
 KS102EW = pd.read_csv(r'C:\Users\charl\OneDrive\Documents\2011 Census\KS102EW.csv')
 
 # for col in KS102EW.columns:
 #     print(col)
 
-sql_connect = sqlite3.connect('Census_2011_database.db')
-c = sql_connect.cursor()
+conn = sqlite3.connect('Census_2011_database.db')
+c = conn.cursor()
 
-KS101EW.to_sql("KS101EW", sql_connect, if_exists='append', index=False)
-KS102EW.to_sql("KS102EW", sql_connect, if_exists='append', index=False)
+KS101EW.to_sql("all_data", conn, if_exists='append', index=False)
+KS101EW.to_sql("KS101EW", conn, if_exists='append', index=False)
+KS102EW.to_sql("KS102EW", conn, if_exists='append', index=False)
 
-c.execute("SELECT * FROM KS102EW")
+c.execute("""
+SELECT *
+FROM all_data
+INNER JOIN KS102EW
+ON all_data.geography = KS102EW.geography
+""")
 print(c.fetchall())
 
-# c.execute('''
-# CREATE TABLE KS101EW(
-# date INTEGER,
-# geography VARCHAR,
-# geography_code VARCHAR
-# rural_urban VARCHAR,
-# All_usual_residents INTEGER,
-# Males INTEGER,
-# Females INTEGER,
-# Lives_in_a_household INTEGER,
-# Lives_in_a_communal_establishment INTEGER,
-# Student VARCHAR,
-# Area_Hectares DOUBLE,
-# Density_per_Hectare DOUBLE);
-# ''')
-#
-# contents = csv.reader(open(r'C:\Users\charl\OneDrive\Documents\2011 Census\KS101EW.csv'))
-#
-# c.executemany("INSERT INTO KS101EW VALUES", contents)
+# print(pd.read_sql_query("SELECT * FROM all_data", conn))
 
-sql_connect.commit()
-sql_connect.close()
 
+conn.commit()
+conn.close()
